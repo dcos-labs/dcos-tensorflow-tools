4ff9d4e702376884f43252b624b37eb119ad14c4
Sam Pringle <15springle@gmail.com>
2017-11-02 09:58:16 -0700

    Update mnist.py
    - add replica_device_setter
    - restrict summaries to chief worker
    - reformate file

--- examples/source/mnist/mnist.py
+++ examples/source/mnist/mnist.py
@@ -24,34 +24,39 @@ def main(server, log_dir, context):
     num_training_steps = context.get('num_training_steps') or 1000000
 
     # Import data
-    data_dir = log_dir + '/data'
+    data_dir = './MNIST_DATA'
     mnist = input_data.read_data_sets(data_dir, one_hot=True)
 
-    # Create the model
-    x = tf.placeholder(tf.float32, [None, 784])
-    W = tf.Variable(tf.zeros([784, 10]))
-    b = tf.Variable(tf.zeros([10]))
-    y = tf.matmul(x, W) + b
-    y_ = tf.placeholder(tf.float32, [None, 10])
-    cross_entropy = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-    global_step = tf.contrib.framework.get_or_create_global_step()
-    train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(
-        cross_entropy, global_step=global_step)
-    hooks = [tf.train.StopAtStepHook(last_step=num_training_steps)]
+    with tf.device(tf.train.replica_device_setter(
+                    worker_device="/job:worker/task:%d" %
+                    server.server_def.task_index,
+                    cluster=server.server_def.cluster)):
 
-    # Evaluate the model
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        # Create the model
+        x = tf.placeholder(tf.float32, [None, 784])
+        W = tf.Variable(tf.zeros([784, 10]))
+        b = tf.Variable(tf.zeros([10]))
+        y = tf.matmul(x, W) + b
+        y_ = tf.placeholder(tf.float32, [None, 10])
+        cross_entropy = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+        global_step = tf.contrib.framework.get_or_create_global_step()
+        train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(
+            cross_entropy, global_step=global_step)
+        hooks = [tf.train.StopAtStepHook(last_step=num_training_steps)]
 
-    # Add summaries
-    tf.summary.scalar('accuracy', accuracy)
-    merged = tf.summary.merge_all()
-    train_writer = tf.summary.FileWriter(log_dir + '/train', tf.get_default_graph())
-    test_writer = tf.summary.FileWriter(log_dir + '/test')
+        # Evaluate the model
+        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+        # Add summaries
+        tf.summary.scalar('accuracy', accuracy)
+        merged = tf.summary.merge_all()
 
     # Begin distributed training
     is_chief = server.server_def.task_index == 0
+    train_writer = tf.summary.FileWriter(log_dir + '/train', tf.get_default_graph()) if is_chief else None
+    test_writer = tf.summary.FileWriter(log_dir + '/test') if is_chief else None
     with tf.train.MonitoredTrainingSession(master=server.target,
                                            is_chief=is_chief,
                                            hooks=hooks) as mon_sess:
@@ -59,15 +64,16 @@ def main(server, log_dir, context):
         while not mon_sess.should_stop():
             if local_step % 10 == 0:
                 eval_xs, eval_ys = mnist.test.images, mnist.test.labels
-                summary, acc = mon_sess.run([merged, accuracy],
-                                            feed_dict={x: eval_xs, y_: eval_ys})
-                test_writer.add_summary(summary, local_step)
+                summary, acc = mon_sess.run([merged, accuracy], feed_dict={x: eval_xs, y_: eval_ys})
                 print('Accuracy at global step {} (local step {}): {}'.format(
-                    mon_sess.run(global_step), local_step, acc))
+                      mon_sess.run(global_step), local_step, acc))
+                if is_chief:
+                    test_writer.add_summary(summary, local_step)
             train_xs, train_ys = mnist.train.next_batch(100)
-            summary, _ = mon_sess.run([merged, train_op],
-                                      feed_dict={x: train_xs, y_: train_ys})
-            train_writer.add_summary(summary, local_step)
+            summary, _ = mon_sess.run([merged, train_op], feed_dict={x: train_xs, y_: train_ys})
+            if is_chief:
+                train_writer.add_summary(summary, local_step)
             local_step += 1
-        train_writer.close()
-        test_writer.close()
+        if is_chief:
+            train_writer.close()
+            test_writer.close()
